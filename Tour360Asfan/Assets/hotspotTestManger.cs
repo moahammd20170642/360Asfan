6cdfde23ed47becf06bd1fd114da5263c6e7c48f
salah <msalah999@gmail.com>
2024-04-04 14:44:12 +0300

    t

--- Tour360Asfan/Assets/hotspotTestManger.cs
+++ Tour360Asfan/Assets/hotspotTestManger.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 
 public class hotspotTestManger : MonoBehaviour
 {
-    public int sphereIndex ;
+    public int sphereIndex;
+    public GameObject table;
+    datamanger datamangerf;
+    public GameObject world;
+    void OnMouseDown()
+    {
+        // Perform your desired action here
+        Debug.Log("Object Clicked!");
+
+        // For example, let's change the object's color
+        GetComponent<Renderer>().material.color = Color.red;
+        datamangerf.changeImages(sphereIndex - 1);
+
+        table.SetActive(false);
+        world.SetActive(true);
+    }
+
+
+    private void Start()
+    {
+        datamangerf = GameObject.Find("DataManager").GetComponent<datamanger>();
+    }
 }
+
+
+
