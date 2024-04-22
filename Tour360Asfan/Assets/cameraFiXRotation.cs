fc8782da22edf7329c859bc0440ff32a9ff00afd
salah <msalah999@gmail.com>
2024-04-22 17:18:21 +0300

    toto

--- Tour360Asfan/Assets/cameraFiXRotation.cs
+++ Tour360Asfan/Assets/cameraFiXRotation.cs
@@ -8,14 +8,14 @@ public class cameraFiXRotation : MonoBehaviour
     public Transform transformO;
     // Start is called before the first frame update
 
-    //public bool itNeedsFix = false;
-    public quaternion quaternion;
-    //private void OnEnable()
-    //{
-    //    //if (itNeedsFix)
-    //    //{
-    //       transformO.rotation = quaternion;
-    //    //}
-        
-    //}
+    public bool itNeedsFix = false;
+    public float y_axix;
+    private void OnEnable()
+    {
+        if (itNeedsFix)
+        {
+            transformO.eulerAngles = new Vector3(0, y_axix, 0);
+        }
+
+    }
 }
